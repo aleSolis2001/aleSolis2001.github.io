25ab97cfafba52944e509347e8ee395d279d870a
aleSolis2001 <118699950+aleSolis2001@users.noreply.github.com>
2023-12-08 14:24:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,26 @@ function Calcular() {
 
         if (peso <= 10) {
             hidratacionBasal = peso * 100;
+            const mantenimientoMedio = hidratacionBasal * 1.5
         } else if (peso <= 20) {
             hidratacionBasal = 1000 + (peso - 10) * 50;
+            const mantenimientoMedio = hidratacionBasal * 1.5
         } else if (peso <= 30) {
             hidratacionBasal = 1000 + 500 + (peso - 20) * 20;
+            const mantenimientoMedio = hidratacionBasal * 1.5 
         } else {
             const sC = ( (peso * 4) + 7) / (peso + 90)
             const hidratacionBasal1500 = Math.round(sC * 1500)
             const hidratacionBasal2000 = Math.round(sC * 2000)
             resultadoOutput.innerHTML = "Volumen diario: <span class='resaltar'>" + `${hidratacionBasal1500} - ${hidratacionBasal2000}` + "</span> cc";
             mantenimiento.innerHTML = "Mantenimiento: <span class='resaltar'>" + `${(hidratacionBasal1500 / 24).toFixed(1)} - ${(hidratacionBasal2000 / 24).toFixed(1)} ` + "</span> cc/hr";
-            mMedio.innerHTML = "m + m/2: <span class='resaltar'>" + `${(hidratacionBasal1500 + hidratacionBasal1500 / 2)} - ${(hidratacionBasal2000 + hidratacionBasal2000 / 2)} ` + "</span> cc/hr";
+            mMedio.innerHTML = "m + m/2: <span class='resaltar'>" + `${(hidratacionBasal1500 / 24 * 1.5).toFixed(1)} - ${(hidratacionBasal2000 / 24 * 1.5).toFixed(1)}` + "</span> cc/hr";
             return;
         }
 
         resultadoOutput.innerHTML = "Volumen diario: <span class='resaltar'>" + hidratacionBasal + "</span> cc";
         mantenimiento.innerHTML = "Mantenimiento: <span class='resaltar'>" + (hidratacionBasal / 24).toFixed(1) + "</span> cc/hr"
-        mMedio.innerHTML = "m + m/2: <span class='resaltar'>" + (hidratacionBasal + hidratacionBasal / 2) + "</span> cc/hr"
+        mMedio.innerHTML = "m + m/2: <span class='resaltar'>" + (hidratacionBasal / 24 * 1.5).toFixed(1) + "</span> cc/hr"
     } else {
         resultadoOutput.innerHTML = "Por favor, ingrese un peso válido"
     }
